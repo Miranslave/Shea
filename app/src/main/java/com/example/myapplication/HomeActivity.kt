f7d5826d52aeeb55344aeff4a9ccef2657a88060
Killian <killian.denechere@gmail.com>
2023-11-09 18:26:02 -0500

    Add tab switch from home to search (not the others yet)

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,11 +1,44 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+private val tabsFragments = mapOf(
+    R.id.home_tab to mapOf(
+        "fragment" to HomeFragment(),
+        "title" to "Accueil"
+    ),
+    R.id.search_tab to mapOf(
+        "fragment" to SearchFragment(),
+        "title" to "Recherche"
+    )
+)
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        arrayOf(
+            R.id.home_tab,
+            R.id.search_tab).forEach { id -> findViewById<View>(id).setOnClickListener { changeToTab(id) }
+        }
+    }
+
+    private fun changeToTab(id: Int) {
+        val tab = tabsFragments[id]
+
+        // Change the title
+        findViewById<TextView>(R.id.titleText).text = tab?.get("title") as String
+
+        // Change the fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.home_fragments_container, tab?.get("fragment") as Fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
