ca2366a85a51932e7bc65fa9f1b07c6a02bf423c
Killian <killian.denechere@gmail.com>
2023-11-10 14:28:22 -0500

    Add tab changes for all tabs

--- app/src/main/java/com/example/myapplication/BaseActivity.kt
+++ app/src/main/java/com/example/myapplication/BaseActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import kotlin.reflect.typeOf
 
-private val tabsFragments = mapOf(
+private val tabs = mapOf(
     R.id.home_tab to mapOf(
-        "title" to "Accueil"
-    ),
-    R.id.search_tab to mapOf(
-        "title" to "Recherche"
+        "fragment" to HomeFragment(), "title" to "Accueil"
+    ), R.id.search_tab to mapOf(
+        "fragment" to SearchFragment(), "title" to "Recherche"
+    ), R.id.library_tab to mapOf(
+        "fragment" to LibraryFragment(), "title" to "Bibliothèque"
+    ), R.id.settings_tab to mapOf(
+        "fragment" to SettingsFragment(), "title" to "Paramètres"
     )
 )
 
@@ -20,27 +25,26 @@ class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
 
-        arrayOf(
-            R.id.home_tab,
-            R.id.search_tab).forEach { id -> findViewById<View>(id).setOnClickListener { changeToTab(id) }
+        tabs.keys.forEach { id ->
+            findViewById<View>(id).setOnClickListener { changeToTab(id) }
         }
     }
 
     private fun changeToTab(id: Int) {
-        val tab = tabsFragments[id]
+        val tab = tabs[id]
 
         // Change the title
         findViewById<TextView>(R.id.titleText).text = tab?.get("title") as String
 
         // Change the fragment
-        val newFragment = when(id) {
-            R.id.home_tab -> HomeFragment()
-            R.id.search_tab -> SearchFragment()
-            else -> throw IllegalArgumentException("Invalid tab ID")
-        }
+//        val newFragment = when(id) {
+//            R.id.home_tab -> HomeFragment()
+//            R.id.search_tab -> SearchFragment()
+//            else -> throw IllegalArgumentException("Invalid tab ID")
+//        }
 
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.home_fragments_container, newFragment)
+        transaction.replace(R.id.home_fragments_container, tab["fragment"] as Fragment)
         transaction.addToBackStack(null)
         transaction.commit()
     }
